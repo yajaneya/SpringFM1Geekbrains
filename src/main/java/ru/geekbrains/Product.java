2ab160e7d61a811255e6165f5bac7c2e1c468daa
yajaneya <janeya@yandex.ru>
2021-11-02 21:55:38 +0300

    Создан класс Product.

---
+++ src/main/java/ru/geekbrains/Product.java
@@ -0,0 +1,31 @@
+package ru.geekbrains;
+
+public class Product {
+
+    private int id;
+    private String title;
+    private int cost;
+
+    public Product(int id, String title, int cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product: '" + title + "' id: " + id + ", cost = " + cost;
+    }
+}
